c41990499c7385e5831ab176233cab024118cb13
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-16 19:36:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function dayOfAYear(year) {
+	let days=0;
+	if(year%4==0 && year%100!=0 || year%400 ==0)
+	{
+		days=366;
+	}
+	else
+	{
+		days=365;
+	}
+	return days;
+}
